9f103354c0981b73a1ddf8f87142d985ea51efa6
Zaldy <worranter@gmail.com>
2019-09-22 19:10:04 +0700

    Intial Commit

--- PBO2_ZaldyAuw/src/com/zaldy/Main.java
+++ PBO2_ZaldyAuw/src/com/zaldy/Main.java
@@ -10,12 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/ModulForm.fxml"));
-        primaryStage.setTitle("Praktek minggu 2");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("view/MainForm.fxml"));
+        primaryStage.setTitle("1772031");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
